3624456b2869e79552d3b25978c4912e28b9d42f
JesusAntonioSG12 <jesusantonio1506@gmail.com>
2025-09-04 17:33:57 -0600

    Add examples for pass, break, and continue statements in Python

---
+++ 06-break_continue_pass.py
@@ -0,0 +1,19 @@
+
+# # Pass
+# for item in range(6):
+#     pass
+
+# # Break
+# for i in range(10):
+#     if i == 5:
+#         break
+#     print(i)
+
+# Continue
+number = 0
+while number in range(5):
+    number += 1
+    continue
+    print(number)
+
+print(number)
